7b6c8ed6ed257d6a71971aa7c448add0c5d06e7d
24LP4781 24LP4781 <24LP4781@G401-CC0119.local>
2024-06-18 18:15:27 +0800

    todo

---
+++ main.js
@@ -0,0 +1,6 @@
+function addTask() {
+    const input = document.getElementById("input");
+    console.log(input.value);
+    value.innerHTML = value.innerHTML + "<div>" + input.value + "</div>";
+}
+
